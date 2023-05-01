8360fe3e6b739c0bca0694294399e09d0d8377f4
Matheus Bastos <matheus.mab432@gmail.com>
2023-05-01 12:30:35 -0300

    Adding auth-page-layout

---
+++ src/app/components/layout/auth-page-layout/auth-page-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-page-layout',
+  templateUrl: './auth-page-layout.component.html',
+  styleUrls: ['./auth-page-layout.component.scss'],
+})
+export class AuthPageLayoutComponent {}
--- src/app/pages/authentication/authentication.module.ts
+++ src/app/pages/authentication/authentication.module.ts
@@ -7,9 +7,16 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginFormComponent } from 'src/app/components/authentication/login-form/login-form.component';
 import { SignupFormComponent } from 'src/app/components/authentication/signup-form/signup-form.component';
 import { CrudPageModule } from 'src/app/shared/crud-page.module';
+import { AuthPageLayoutComponent } from 'src/app/components/layout/auth-page-layout/auth-page-layout.component';
 
 @NgModule({
-  declarations: [LoginComponent, SignupComponent, LoginFormComponent, SignupFormComponent],
+  declarations: [
+    LoginComponent,
+    SignupComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    AuthPageLayoutComponent,
+  ],
   imports: [CommonModule, AuthenticationRoutingModule, PageModule, CrudPageModule],
   exports: [LoginFormComponent, SignupFormComponent],
 })
